efd5303fcee0e3b103884f060c0bd799bfe660d4
Archiruz <47630429+Archiruz@users.noreply.github.com>
2020-02-06 21:27:05 +0700

    fixed again lol

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -154,7 +154,9 @@ async def type_afk_is_not_true(notafk):
     global AFKREASON
     if ISAFK:
         ISAFK = False
-        await notafk.respond("I'm no longer AFK.\nThis auto-generated message will be deleted immediately")
+        msg = await notafk.respond("I'm no longer AFK.\nThis auto-generated message will be deleted immediately")
+        await sleep(2)
+        await msg.delete()
         if BOTLOG:
             await notafk.client.send_message(
                 BOTLOG_CHATID,
